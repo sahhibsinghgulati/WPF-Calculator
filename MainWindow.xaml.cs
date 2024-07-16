62b2d0ff8041d8f3fff9f5e1508f84458278b4fe
sahhibisnghgulati <sahhib11246@gmail.com>
2024-07-16 23:08:38 +0530

    Added Del Button and other things

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,9 @@ namespace WPF_Calculator
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    { 
+        string output="";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -24,7 +26,55 @@ namespace WPF_Calculator
         private void numBtn_Click(object sender, RoutedEventArgs e)
         {
             string name = ((Button)sender).Name;
-            MessageBox.Show(name);
+
+            switch (name)
+            {
+                case "OneBtn":
+                output += "1";
+                    Output.Text = output;
+                    break;
+                case "TwoBtn":
+                    output += "2";
+                    Output.Text = output;
+                    break;
+                case "ThreeBtn":
+                    output += "3";
+                    Output.Text = output;
+                    break;
+                case "FourBtn":
+                    output += "4";
+                    Output.Text = output;
+                    break;
+                case "FiveBtn":
+                    output += "5";
+                    Output.Text = output;
+                    break;
+                case "SixBtn":
+                    output += "6";
+                    Output.Text = output;
+                    break;
+                case "SevenBtn":
+                    output += "7";
+                    Output.Text = output;
+                    break;
+                case "EightBtn":
+                    output += "8";
+                    Output.Text = output;
+                    break;
+                case "NineBtn":
+                    output += "9";
+                    Output.Text = output;
+                    break;
+                case "ZeroBtn":
+                    output += "0";
+                    Output.Text = output;
+                    break;
+                case "DelBtn":
+                    output = output.Substring(0,output.Length-1);
+                    Output.Text = output;
+                    break;
+
+            }
         }
     }
 }
